a4c9f1f1d32114e9cc6172d627bc3586b093431a
Adarsh <adarsht3322@gmail.com>
2022-11-16 15:20:28 +0530

    init

---
+++ app/app/calc.py
@@ -0,0 +1,9 @@
+"""
+calculator funcions
+"""
+
+def add(x,y):
+    return x+y
+
+def sub(x,y):
+    return x-y
---
+++ app/tests.py
@@ -0,0 +1,20 @@
+# sample test
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTest(SimpleTestCase):
+
+    def test_add_numbers(self):
+        """test adding numbers together"""
+        res =  calc.add(5,6)
+
+        self.assertEqual(res, 11)
+
+    def test_sub_numbers(self):
+        """test suntracting numbers"""
+
+        res = calc.sub(15,10)
+
+        self.assertEqual(res,5)
