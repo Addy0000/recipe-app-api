400b66249ad6ab0b72bb5b89bfff2152df06ff9d
Adarsh <adarsht3322@gmail.com>
2022-11-20 19:47:36 +0530

    init

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -21,3 +21,13 @@ class CommandTests(SimpleTestCase):
         call_command('wait_for_db')
 
         patched_check.assert_called_once_with(databases=['default'])
+
+@patch('time.sleep')
+def test_wait_for_db_delay(self,patched_sleep,patched_check):
+    patched_check.side_effect = [psycopg2Error]*2+\
+        [OperationalError] * 3 + [True]
+
+    call_command('wait_for_db')
+
+    self.assertEqual(patched_check.call_count,6)
+    patched_check.assert_called_with(databases=['default'])
