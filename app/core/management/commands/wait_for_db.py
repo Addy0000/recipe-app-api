62b3b2082e2a94c1cb9187a1e7691cb4b785946b
Adarsh <adarsht3322@gmail.com>
2022-11-21 14:17:03 +0530

    changes in indentations

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -2,22 +2,24 @@
 Django command to wait for the database to be available.
 """
 from django.core.management.base import BaseCommand
-import time 
+import time
 from psycopg2 import OperationalError as Psycopg2Error
 from django.db.utils import OperationalError
+
 
 class Command(BaseCommand):
     """"Django command to wait for db"""
 
-    def handle(self,*args,**options):
+    def handle(self, *args, **options):
         self.stdout.write('Waiting for database...')
-        db_up= False
+        db_up = False
+
         while db_up is False:
             try:
                 self.check(databases=['default'])
-                db_up=True
+                db_up = True
 
-            except(Psycopg2Error,OperationalError):
+            except(Psycopg2Error, OperationalError):
                 self.stdout.write('database unavailable waiting 1 second...')
                 time.sleep(1)
 
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -2,13 +2,12 @@
 Test custom django management commands
 """
 
-from unittest.mock import patch 
-
-from psycopg2 import OperationalError as Psycopg2Error 
-
+from unittest.mock import patch
+from psycopg2 import OperationalError as Psycopg2Error
 from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
+
 
 @patch("core.management.commands.wait_for_db.Command.check")
 class CommandTests(SimpleTestCase):
@@ -22,12 +21,13 @@ class CommandTests(SimpleTestCase):
 
         patched_check.assert_called_once_with(databases=['default'])
 
+
 @patch('time.sleep')
-def test_wait_for_db_delay(self,patched_sleep,patched_check):
-    patched_check.side_effect = [Psycopg2Error]*2+\
+def test_wait_for_db_delay(self, patched_sleep, patched_check):
+    patched_check.side_effect = [Psycopg2Error] * 2 +\
         [OperationalError] * 3 + [True]
 
     call_command('wait_for_db')
 
-    self.assertEqual(patched_check.call_count,6)
+    self.assertEqual(patched_check.call_count, 6)
     patched_check.assert_called_with(databases=['default'])
